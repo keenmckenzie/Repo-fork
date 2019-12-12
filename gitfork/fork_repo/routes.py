7c52567f46e80f5fb184a44a303f5b7ba8d02f5c
Keenan McKenzie <keenan@mckenziemail.com>
2019-12-12 06:47:39 -0600

    update login callback route to fork repo. not working

--- gitfork/fork_repo/routes.py
+++ gitfork/fork_repo/routes.py
@@ -10,7 +10,7 @@ def test_route():
 
 @mod.route('/login')
 def login():
-   client_id = 'app client id from github'
+   client_id = 'a199df7116a41d717d94'
    authorization_base_url = 'https://github.com/login/oauth/authorize'
    github = OAuth2Session(client_id)
    authorization_url, state = github.authorization_url(authorization_base_url)
@@ -18,12 +18,18 @@ def login():
 
 @mod.route('/login/githubRedirect')
 def auth():
-   client_id = 'app client id from github'
-   client_secret = 'app client secret from github'
+   client_id = 'client id from github'
+   client_secret = 'client secret from github'
    redirect_response = request.url
    github = OAuth2Session(client_id)
    token_url = 'https://github.com/login/oauth/access_token'
    github.fetch_token(token_url, client_secret=client_secret, authorization_response=redirect_response)
-   r = github.get('https://api.github.com/user')
-   return r.content
+   r = github.post('https://api.github.com/repos/marblexu/PythonPlantsVsZombies/forks')
+   return r
+
+@mod.route('/fork-repo/<string:user_name>/<string:repo_name>')
+def fork(user_name, repo_name):
+   client_id = 'client id from github'
+   github = OAuth2Session(client_id)
+   r = github.get('https://api.github.com/repos/'+user_name+ '/' + repo_name + '/forks')
  
